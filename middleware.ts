3e90bbd6b209fa99e83ef8ed88b7749b0a879f32
franklyngomes <franklyngomes15@gmail.com>
2025-03-25 12:35:15 +0530

    Completed

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,17 @@
-import {NextRequest, NextResponse} from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+  runtime: "edge",
+  matcher: ["/home", "/create", "/"],
+};
 export const middleware = (request: NextRequest) => {
-    const token = request.cookies.get("user_token")
+  const token = request.cookies.get("user_token");
 
-    if(!token){
-        return NextResponse.redirect(new URL("/signin", request.url))
-    }
-    console.log("User Found")
-    if(request.nextUrl.pathname === "/"){
-        return NextResponse.redirect(new URL("/home", request.url))
-    }
-    return NextResponse.next()
-}
-export const config ={
-    matcher: [
-        "/home",
-        "/create",
-        "/"
-    ]
-}
+  if (!token) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+  return NextResponse.next();
+};
